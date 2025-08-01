1d67167cf1c3d0fe1dd5b4f9b20bc720f3ad53f3
panelenelo <panelenelo@gmail.com>
2025-08-01 11:52:57 -0300

    binary search

--- searching/binarysearch.hpp
+++ searching/binarysearch.hpp
@@ -2,7 +2,24 @@
 
 int binarySearch_me(vector<int> &toSearch, int searched){
 
-    int searched;
+    int left = 0;
+    int right = toSearch.size()-1;
+    int mid = (left+right)/2;
 
-    return searched;
+    while(left <= right){
+
+        if(toSearch[mid] == searched){
+            return mid;
+        }
+
+        if(toSearch[mid] < searched){
+            left = mid+1;
+        }else{
+            right = mid-1;
+        }
+
+        mid = (left+right)/2;
+    }
+
+    return -1;
 }
--- searching/searchings.cpp
+++ searching/searchings.cpp
@@ -9,15 +9,24 @@ int main(){
     vector<int> toSearch = rand_vector(10);
     mergesort_me(toSearch);
 
-    int searched;
-
     for(int i: toSearch){
         cout << i << endl;
     }
+    cout << endl;
 
+    int searched, index;
+    cin >> searched;
+    
     cout << "---------" << endl;
 
-    binarySearch_me(toSearch, searched);
+
+    index = binarySearch_me(toSearch, searched);
+
+    if(index != -1){
+        cout << "number: " << searched << " found at index: " << index << endl;
+    }else{
+        cout << "number not found on list given" << endl;
+    }
 
     cout << "---------" << endl;
     
